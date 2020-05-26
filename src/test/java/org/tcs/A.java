d10ad55e82df760b8aae2ac0fab45b2e0d3ba50f
Vinodh353 <saivinodh120@gmail.com>
2020-05-26 13:06:56 +0530

    my work is done

--- src/test/java/org/tcs/A.java
+++ src/test/java/org/tcs/A.java
@@ -6,6 +6,8 @@ public static void main(String[] args) {
 	System.out.println("vinod");
 	System.out.println("Hello");
 	System.out.println("Welcome");
+	System.out.println("java");
+	System.out.println("selenium");
 	}
 
 }
